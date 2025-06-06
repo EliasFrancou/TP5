b58a55d071fb5712d642801db5b9371c86abedcb
EliasFrancou <88948420+EliasFrancou@users.noreply.github.com>
2025-06-06 11:59:55 -0300

    cambios
    arreglo errores

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,23 +26,3 @@ const Home = async () => {
 }
 
 export default Home;
-
-//       ${
-//         characters?.results
-//           ? characters.results.map(character => `
-//             <article class="Character-item">
-//               <a href="#/${character.id}/">
-//                 <img src="${character.image}" alt="${character.name}">
-//                 <h2>${character.name}</h2>
-//               </a>
-//             </article>
-//           `).join('')
-//           : '<p>No hay personajes disponibles.</p>'
-//       }
-//     </div>
-//   `;
-  
-//   return view;
-// }
-
-// export default Home;
